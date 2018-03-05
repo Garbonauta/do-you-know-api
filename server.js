4c4109442dcdad4e1de1e839dc8d3dfa55abd226
Garbonauta <eduardo.moreno.j@gmail.com>
2018-03-05 13:03:16 -0600

    Heroku set up

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ import AuthRoutes from './src/plugins/AuthRoutes'
 import {mount} from './src/config/Key'
 
 const server = new Hapi.server({
-  port: process.env.SERVER_PORT,
-  host: process.env.SERVER_HOST,
+  port: process.env.SERVER_PORT || 3000,
   routes: {
     cors: true,
   },
