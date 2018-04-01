15f0db91e110e66b212227e07fffb4ee52be3235
Garbonauta <eduardo.moreno.j@gmail.com>
2018-03-31 21:22:38 -0500

    Date is now being provided client side. This may change but I had bugs.

--- src/models/repositories/groupPost.js
+++ src/models/repositories/groupPost.js
@@ -2,18 +2,19 @@ import { GroupPost, Post } from 'models/models'
 import mongoose from 'mongoose'
 
 export async function getGroupPosts(groupId) {
-  const posts = await GroupPost.findById(groupId, 'posts._id posts.text posts.owner')
+  const posts = await GroupPost.findById(groupId, 'posts._id posts.text posts.owner posts.createdAt')
     .populate('posts.owner', 'info.fullName info.link info.pictures.small');
   return posts.toObject();
 }
 
-export async function insertGroupPost(groupId, {postText, owner}) {
+export async function insertGroupPost(groupId, {postText, owner, createdAt}) {
   let groupPost = await GroupPost.findById(groupId);
 
   const post = new Post({
     _id: mongoose.Types.ObjectId(),
     text: postText,
     owner,
+    createdAt: new Date(createdAt)
   });
 
   if(!groupPost) {
